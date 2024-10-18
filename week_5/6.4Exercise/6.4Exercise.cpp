f464829ae6f34eab8a5a34d2656da18dafdf518e
Klara Ahinta <176823710+kYra3041@users.noreply.github.com>
2024-10-18 07:33:28 +0000

    added process + struct + array

--- week_5/6.4Exercise/6.4Exercise.cpp
+++ week_5/6.4Exercise/6.4Exercise.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
 #include <iomanip>
 
+//struct declaration
+struct Student {
+    int idNum;
+    float midtermExam;
+    float finalExam;
+    float average;
+};
+//variable declaration and initialization
+Student student [50];
 
-
+//main process
 int main () {
+    int n,i;
 
+    std::cout << "Enter the number of students : ";
+    std::cin >> n;
 
+    //input student data
+    for (i=0; i<0; i++) {
+        std::cout << "Enter the NIM : ";
+        std::cin >> student[i].idNum;
+
+        std::cout << "Enter UTS value : ";
+        std::cin >> student[i].midtermExam;
+
+        std::cout << "Enter UAS value : ";
+        std::cin >> student[i].finalExam;
+        
+        //calculate the average
+        student[i].average = (student[i].midtermExam + student[i].finalExam) / 2.0f;
+    }
+
+    //display the result to user
+    std::cout << "\n";
+    for (i=0; i<0; i++) {
+        std::cout << "------\n";
+        std::cout << "nim : " << student[i].idNum;
+        std::cout << "average : " << std::fixed << std::setprecision(2) << student[i].average << "\n";
+    }
+
+    
+    // Pause mechanism
+    std::cout << "\nPress Enter to continue.";
+    std::cin.ignore();
+    std::cin.get();
 
     return 0;
 }
