2aba252104fd3a76497c699c7509f5b5d3c7935d
Klara Ahinta <176823710+kYra3041@users.noreply.github.com>
2024-10-18 06:24:18 +0000

    task1 newfile cpp

---
+++ week_5/Task1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+//struct declaration 
+struct fullname {
+    std::string FirstName;
+    std::string LastName;
+};
+
+struct StudentRec {
+    fullname name;
+    std::string idNum;
+    float GPA;
+};
+
+//variable declaration and initialization
+StudentRec theStudent[10];
+
+//process
+int main () {
+    int n, i; //declaration for n and i
+
+    std::cout << "Enter the number of student : ";
+    std::cin >> n;
+    std::cout << "Student data : " << std::endl;
+    for (i = 0; i < n; ++i) {
+        std::cout << "First Name : ";
+        std::cin >> theStudent[i].name.FirstName;
+
+        std::cout << "Last Name : ";
+        std::cin >> theStudent[i].name.LastName;
+
+        std::cout << "Student ID Number : ";
+        std::cin >> theStudent[i].idNum;
+
+        std::cout << "GPA : ";
+        std::cin >> theStudent[i].GPA;
+        }
+
+//finalization
+    std::cout << "Student Data";
+    for (i=0; i<n; i++) {
+        std::cout << theStudent[i].name.FirstName << " ";
+        std::cout << theStudent[i].name.LastName << " ";
+        std::cout << theStudent[i].idNum << " ";
+        std::cout << theStudent[i].GPA << " ";
+    }
+    std::cout << "Press Enter to continue...";
+    std::cin.ignore();
+    std::cin.get();
+    
+    return 0;
+}
