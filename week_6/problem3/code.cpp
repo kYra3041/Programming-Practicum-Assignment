f5dd28feda24dbf0cc8a45f2e73409f0bf7f347f
Klara Ahinta <176823710+kYra3041@users.noreply.github.com>
2024-10-28 07:33:29 +0000

    main

---
+++ week_6/problem3/code.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int calculateGCD (int a, int b);
+
+int main () {
+    int num1, num2;
+
+    //get input from user
+    std::cout << "Enter two positive number to find their GCD: \n" << std::endl;
+    std::cout << "1st number: ";
+    std::cin >> num1;
+    std::cout << "2nd number: ";
+    std::cin >> num2;
+
+    //input validation
+
+    if (num1 <= 0 || num2 <=0) {
+        
+    }
+
+    return 0;
+}
