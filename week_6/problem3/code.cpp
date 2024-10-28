443ec1a5b0c6b81082c7de6e5c0c6ea2bac176a0
Klara Ahinta <176823710+kYra3041@users.noreply.github.com>
2024-10-28 10:36:34 +0000

    std cout gcd result output

--- week_6/problem3/code.cpp
+++ week_6/problem3/code.cpp
@@ -6,7 +6,7 @@ int main () {
     int num1, num2;
 
     //get input from user
-    std::cout << "Enter two positive number to find their GCD: \n" << std::endl;
+    std::cout << "Enter two positive number to find their GCD: \n";
     std::cout << "1st number: ";
     std::cin >> num1;
     std::cout << "2nd number: ";
@@ -15,9 +15,19 @@ int main () {
     //input validation
 
     if (num1 <= 0 || num2 <=0) {
-        std::cout << "please enter positive integers only :3" << std::endl;
+        std::cout << "please enter positive integers only :D" << std::endl;
         return 1;
     }
 
+    int gcd = calculateGCD(num1, num2);
+    std::cout << "The GCD of " << num1 << " and " << num2 << " is: " << gcd << std::endl;
+
     return 0;
+}
+int calculateGCD (int a, int b) {
+    if (b == 0){
+        return a;
+}
+
+return calculateGCD(b, a%b);
 }
