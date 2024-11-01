2e2f5e3fc12b10061e9b123e60d2c5c7901bb581
Klara Ahinta <176823710+kYra3041@users.noreply.github.com>
2024-11-01 05:21:22 +0000

    sum test edited

---
+++ week_3/Problem_1/SumTest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main () {
+    int n;
+    long long sum = 0; //using long long to handle sum
+    std::string explanation = "(Explanation =";
+
+    std::cout << "Insert a natural number (n) = "; //ask user for input
+    std::cin >> n;
+
+    for (int i = 1; i <= n; ++i) { //Calculating sum and showing explanation
+        sum += i;
+        explanation += " " + std::to_string(i);
+        if (i < n) {;
+            explanation += " +";
+        }
+    }
+    explanation += " = " + std::to_string(sum) + " )";
+
+    //output the result and the explanation
+    std::cout << "Sum = " << sum << std::endl;
+    std::cout << explanation << std::endl;
+
+    return 0;
+}
